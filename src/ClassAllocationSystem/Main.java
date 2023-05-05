7886e1cf6f5beaac130add6c63ee1fd3af9fe5ff
Ahmet ARSLAN <ahmet_1285_arslan@hotmail.com>
2023-05-05 22:34:08 +0300

    Test GitHub 2

---
+++ src/ClassAllocationSystem/Main.java
@@ -0,0 +1,14 @@
+package ClassAllocationSystem;
+
+
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello world");
+
+
+        CommandPattern commandPattern = new CommandPattern();
+        commandPattern.printTest();
+    }
+
+
+}
